dd10fe374c2504da2e24968fea02fce2f205c91a
joao-arthur <joao.lothamer@gmail.com>
2022-09-05 12:55:04 -0300

    chore: create project

---
+++ src/main/java/com/learning_api/Employee.java
@@ -0,0 +1,19 @@
+package com.learning_api;
+
+public class Employee {
+    private final String name;
+    private final int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
